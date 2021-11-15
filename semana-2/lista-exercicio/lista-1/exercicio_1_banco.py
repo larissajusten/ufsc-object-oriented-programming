c844b0f9692c0e4941343f64bd98218469942d54
Larissa Justen <larissajusten@gmail.com>
2021-11-15 01:13:57 -0300

    Add exerc 1 completo

--- semana-2/lista-exercicio/lista-1/exercicio_1_banco.py
+++ semana-2/lista-exercicio/lista-1/exercicio_1_banco.py
@@ -15,6 +15,8 @@ Exercício 1: Banco
         e um resumo com todas as informações da conta e seus respectivos clientes.
 """
 from abc import ABC, abstractmethod
+from datetime import datetime
+import re
 
 class Cliente:
     def __init__(self, nome, telefone):
@@ -68,32 +70,111 @@ class Banco:
     def AdicionarContaPoupanca(self, novaConta):
         self.contasPoupança = novaConta
 
-class ContaCorrente:
+    def VisualizarClientes(self):
+        for conta in self.contasPoupança + self.contasCorrente + self.contasEspeciais:
+            conta.VisualizarTitulares()
+
+    def VisualizarInformacoesContas(self):
+        print('')
+        print('# CONTAS POUPANCA')
+        for i, conta in enumerate(self.contasPoupança):
+            print(f'Conta {i}')
+            print(f'[Saldo]: {conta.saldo}')
+            conta.VisualizarTitulares()
+        
+        print('')
+        print('# CONTAS CORRENTE')
+        for i, conta in enumerate(self.contasCorrente):
+
+            print(f'Conta {i+len(self.contasPoupança)}')
+            print(f'[Saldo]: {conta.saldo}')
+            conta.VisualizarTitulares()
+
+        print('')
+        print('# CONTAS ESPECIAIS')
+        for i, conta in enumerate(self.contasEspeciais):
+            print(f'Conta {i+len(self.contasPoupança)+len(self.contasCorrente)}')
+            print(f'[Saldo]: {conta.saldo}')
+            conta.VisualizarTitulares()
+
+    def ProcurarConta(self, nome):
+        for conta in self.contasPoupança + self.contasCorrente + self.contasEspeciais:
+            conta_encontrada = [conta for titular in conta.titulares if nome.lower() in titular.nome.lower()]
+            if conta_encontrada: 
+                return conta_encontrada[0]
+        return False
+
+    def ProcurarCliente(self, nome):
+        for conta in self.contasPoupança + self.contasCorrente + self.contasEspeciais:
+            cliente = [titular.nome for titular in conta.titulares if nome.lower() in titular.nome.lower()]
+            if cliente: 
+                return cliente[0]
+        return False
+
+class Conta(ABC):
     def __init__(self, titulares):
         self.__saldo = 0
         self.__titulares = titulares
         self.__extrato = []
-        self.__limite_especial = 400
 
     @property
     def saldo(self):
         return self.__saldo
 
-    @saldo.setter
-    def saldo(self, valor):
-        self.__saldo = valor
+    @property
+    def extrato(self):
+        return self.__extrato
 
     @property
     def titulares(self):
         return self.__titulares
 
-    @property
-    def extrato(self):
-        return self.__extrato
+    @saldo.setter
+    def saldo(self, valor):
+        self.__saldo = valor
 
     @extrato.setter
     def extrato(self, nova_operacao):
         self.extrato.append(nova_operacao)
+
+    @abstractmethod
+    def InformacoesEspecificasDaConta(self):
+        print(f'# Saldo: {self.saldo}')
+
+    def Sacar(self, valor):
+        if valor < 0:
+            raise ValueError('# Valor invalido!')
+        elif valor > self.saldo:
+            print('# Saldo insuficiente! Não foi possivel realizar a operação.')
+        else:
+            self.saldo = self.saldo - valor
+            print(f'# Saque de R$ {valor} realizado com sucesso!')
+            print(f'# Saldo: {self.saldo}')
+            self.extrato = f'# (Saldo) Saque de R$ {valor} -> {round(self.saldo, 2)}'
+
+    def Depositar(self, valor):
+        if valor > 0:
+            self.saldo = self.saldo + valor
+            print(f'# Deposito de R$ {valor} realizado com sucesso!')
+            print(f'# Saldo: {self.saldo}')
+            self.extrato = f'# (Saldo) Deposito de R$ {valor} -> {self.saldo}'
+        else:
+            raise ValueError('# Valor invalido!')
+
+    def TirarExtrato(self):
+        print('# Extrato:')
+        print(f'# Saldo: {self.saldo}')
+        for i, operacao in enumerate(self.extrato):
+            print(f'[{i}]: {operacao}')
+
+    def VisualizarTitulares(self):
+        for titular in self.titulares:
+            print(f'[{titular.nome}]: {titular.telefone}')
+
+class ContaEspecial(Conta):
+    def __init__(self, titulares):
+        super().__init__(titulares)
+        self.__limite_especial = 400
 
     @property
     def limite_especial(self):
@@ -103,55 +184,199 @@ class ContaCorrente:
     def limite_especial(self, novo_limite_especial):
         self.__limite_especial = novo_limite_especial
 
-    def Depositar(self, valor):
-        if valor > 0:
-            self.saldo = self.saldo + valor
-            self.extrato = f'# Deposito de {valor}                       |      {self.saldo}'
-        else:
-            raise ValueError('!Valor invalido!')
+    def InformacoesEspecificasDaConta(self):
+        print(f'# Saldo: {self.saldo} - Limite Especial: {self.limite_especial}')
 
     def Sacar(self, valor):
         if valor < 0:
-            raise ValueError('Valor invalido!')
-        elif valor > self.saldo and (valor - self.saldo) < self.limite_especial:
+            raise ValueError('# Valor invalido!')
+        elif (valor > self.saldo and self.limite_especial > (valor - self.saldo)) or valor == (self.saldo + self.limite_especial):
             resp = input('> Deseja utilizar o limite especial? [S/N]: ')[0].upper()
             if resp == 'S':
-                self.SacarLimiteEspecial(valor - self.saldo)
-                self.extrato = f'# Saque de {valor}                            |      {round(self.saldo, 2)}'
-                self.extrato = f'# Saque de {round(valor - self.saldo, 2)} no limite especial        |      {round(self.limite_especial, 2)}'
+                self.__SacarLimiteEspecial(valor - self.saldo)
+                if self.saldo != 0:
+                    self.extrato = f'# (Saldo) Saque de R$ {valor} -> {round(self.saldo, 2)}'
+                self.extrato = f'# (Limite Especial) Saque de {round(valor - self.saldo, 2)} no limite especial -> {round(self.limite_especial, 2)}'
                 self.saldo = 0
+
+                print(f'# Saque de R$ {valor} realizado com sucesso!')
+                print(f'# Saldo: {self.saldo} - Limite Especial: {self.limite_especial}')
             else:
-                print('Limite insuficiente! Não foi possivel realizar a operação. Por favor tente novamente.')
+                print('# Saldo insuficiente! Não foi possivel realizar a operação.')
         elif valor > self.saldo:
-            print('Limite insuficiente! Não foi possivel realizar a operação. Por favor tente novamente.')
+            print('# Saldo insuficiente! Não foi possivel realizar a operação.')
         else:
             self.saldo = self.saldo - valor
-            self.extrato = f'# Saque de {valor}                            |      {round(self.saldo, 2)}'
+            print(f'# Saque de R$ {valor} realizado com sucesso' )
+            print(f'# Saldo: {self.saldo} - Limite Especial: {self.limite_especial}')
+            self.extrato = f'# (Saldo) Saque de R$ {valor} -> {round(self.saldo, 2)}'
 
-    def SacarLimiteEspecial(self, valor):
+    def __SacarLimiteEspecial(self, valor):
         self.limite_especial = self.limite_especial - valor
 
-    def TirarExtrato(self):
-        print('# Extrato:')
-        for i, operacao in enumerate(self.extrato):
-            print(f'[{i}]: {operacao}')
+    def Depositar(self, valor):
+        if valor > 0 and self.limite_especial < 400:
+            valor_faltante_limite_especial = 400 - self.limite_especial
 
-def main():
+            if valor > valor_faltante_limite_especial:
+                self.limite_especial += valor_faltante_limite_especial
+                self.saldo += valor - valor_faltante_limite_especial
+                self.extrato = f'# (Saldo) Deposito de R$ {valor - valor_faltante_limite_especial} no saldo -> {self.saldo}'
+                self.extrato = f'# (Limite Especial) Deposito de R$ {valor_faltante_limite_especial} no limite especial -> {self.limite_especial}'
+            else:
+                self.limite_especial += valor
+                self.extrato = f'# (Saldo) Deposito de R$ {0} no saldo -> {self.saldo}'
+                self.extrato = f'# (Limite Especial) Deposito de R$ {valor} no limite especial -> {self.limite_especial}'
+                
+            print(f'# Deposito de R$ {valor} realizado com sucesso!')
+            print(f'# Saldo: {self.saldo} - Limite Especial: {self.limite_especial}')
+        elif valor > 0:
+            self.saldo = self.saldo + valor
+            print(f'# Deposito de R$ {valor} realizado com sucesso!')
+            print(f'# Saldo: {self.saldo} - Limite Especial: {self.limite_especial}')
+            self.extrato = f'# (Saldo) Deposito de R$ {valor} -> {self.saldo}'
+        else:
+            raise ValueError('# Valor invalido!')
+
+class ContaCorrente(Conta):
+    def __init__(self, titulares):
+        super().__init__(titulares)
+
+    def InformacoesEspecificasDaConta(self):
+        print(f'# Saldo: {self.saldo}')
+    
+class ContaPoupanca(Conta):
+    def __init__(self, titulares, data_de_criacao = datetime.now()):
+        super().__init__(titulares)
+        self.__rendimento = 1.5
+        self.__data_de_criacao = data_de_criacao
+
+    @property
+    def rendimento(self):
+        return self.__rendimento
+
+    @property
+    def data_de_criacao(self):
+        return self.__data_de_criacao
+
+    def InformacoesEspecificasDaConta(self):
+        print(f'# Saldo: {self.saldo} - Data de criacao: {self.data_de_criacao.strftime("%m/%d/%Y")} - Rendimentos: {self.rendimento}')
+
+    def CalcularRendimentoMensal(self):
+        now = datetime.now()
+        meses = (now.year - self.data_de_criacao.year) * 12 + now.month - self.data_de_criacao.month
+        saldo_por_mes = (self.saldo * (self.rendimento/100))
+        self.saldo += (meses * saldo_por_mes)
+
+def PreCriacaoDeClientes():
     clienteJoao = Cliente('Joao', '51 98171 0800')
     clienteMaria = Cliente('Maria', '51 98899 5678')
+    clienteLincoln = Cliente('Lincoln', '51 30980789')
+    clienteGiovana = Cliente('Giovana', '48 99322 8741')
+    clienteJonata = Cliente('Jonata Tyska Carvalho', '51 90801 0801')
+    clienteMateus = Cliente('Mateus Grellert', '51 90800 0800')
+    return [clienteJoao, clienteMaria, clienteLincoln, clienteGiovana, clienteJonata, clienteMateus]
 
-    contaCorreteJoaoMaria = ContaCorrente([clienteJoao, clienteMaria])
+def Menu(tatu, conta):
+    print('# MENU')
+    opcao_escolhida = 0
 
+    while True:
+        print('')
+        print('[1]: Visualizar todas contas do banco tatu')
+        print('[2]: Visualizar todos clientes do banco tatu')
+        print('[3]: Sacar')
+        print('[4]: Depositar')
+        print('[5]: Tirar extrato')
+        print('[6]: Informacoes Especificas da Conta')
+        print('[z]: Sair')
+
+        opcao_escolhida = input('> Escolha uma opcao: ')
+
+        if opcao_escolhida == 'z':
+            exit()
+
+        if opcao_escolhida == '1':
+            tatu.VisualizarInformacoesContas()
+        elif opcao_escolhida == '2':
+            print('')
+            tatu.VisualizarClientes()
+        elif opcao_escolhida == '3':
+            print('')
+            valor = float(input('> Digite um valor que deseja sacar: '))
+            conta.Sacar(valor)
+        elif opcao_escolhida == '4':        
+            print('')
+            valor = float(input('> Digite o valor que deseja depositar: '))
+            conta.Depositar(valor)
+        elif opcao_escolhida == '5':
+            print('')
+            conta.TirarExtrato()        
+        elif opcao_escolhida == '6':
+            print('')
+            conta.InformacoesEspecificasDaConta()
+        else:
+            print('# Opcao invalida!')
+
+def CriarConta(banco):
+    print('# CRIACAO DE CONTA')
+    nome = input('> Digite seu nome: ')
+    telefone = input('> Digite seu telefone com DDD (somente numeros): ')
+    novoCliente = Cliente(nome, telefone)
+
+    tipo_de_conta = ''
+    while tipo_de_conta not in ['corrente', 'poupanca', 'corrente', 'z']:
+        tipo_de_conta = input('> Escolha o tipo de conta: [CORRENTE/POUPANCA/ESPECIAL]').lower()
+        if tipo_de_conta == 'z':
+            exit()
+        elif tipo_de_conta == 'corrente':
+            banco.AdicionarContaCorrente(ContaCorrente([novoCliente]))
+        elif tipo_de_conta == 'poupanca':
+            banco.AdicionarContaPoupanca(ContaPoupanca([novoCliente]))
+        elif tipo_de_conta == 'especial':
+            banco.AdicionarContaEspecial(ContaEspecial([novoCliente]))
+    return nome
+
+def main():
+    print('# BEM VINDO AO BANCO TATU')
+    print('[Pressione Z para sair]')
+    print('')
+
+    sair = False
     tatu = Banco()
-    tatu.AdicionarContaCorrente(contaCorreteJoaoMaria)
 
-    print('[clienteJoao]: nome ', clienteJoao.nome, ' numero: ', clienteJoao.telefone)
-    print('[clienteMaria]: nome ', clienteMaria.nome, ' numero: ', clienteMaria.telefone)
-    print('[contaCorreteJoaoMaria]: saldo ', contaCorreteJoaoMaria.saldo)
-    contaCorreteJoaoMaria.Depositar(50.8)
-    print('[contaCorreteJoaoMaria +50 no saldo]: saldo ', contaCorreteJoaoMaria.saldo)
-    contaCorreteJoaoMaria.Sacar(60)
-    print('[contaCorreteJoaoMaria -50 no saldo]: saldo ', contaCorreteJoaoMaria.saldo)
-    contaCorreteJoaoMaria.TirarExtrato()
+    clientes = PreCriacaoDeClientes()
+    contasPoupanca = ContaPoupanca(clientes[:3], data_de_criacao = datetime(2021,10,14))
+    contasCorrente = ContaCorrente(clientes[3:5])
+    contasEspecial = ContaEspecial(clientes[5:])
+    tatu.AdicionarContaPoupanca(contasPoupanca)
+    tatu.AdicionarContaCorrente(contasCorrente)
+    tatu.AdicionarContaEspecial(contasEspecial)
+
+    nome = ''
+
+    while not sair:
+        verify_exite_conta = ''
+        while verify_exite_conta != 's' and verify_exite_conta != 'n' and verify_exite_conta != 'z':
+            verify_exite_conta = input('# Voce ja tem uma conta: [S/N/Z] ').lower()
+
+        if verify_exite_conta == 's':
+            nome = input('> Digite seu nome e sobrenome: ')
+            cliente = tatu.ProcurarCliente(nome)
+            if cliente:
+                print('')
+                conta = tatu.ProcurarConta(nome)
+                nome_conta = str(re.sub(r"(\w)([A-Z])", r"\1 \2", type(conta).__name__))
+                print(f'# Ola {cliente}! {nome_conta}')
+                conta.InformacoesEspecificasDaConta()
+                print('')
+            else:
+                print('# Cliente nao encontrado!')
+        elif verify_exite_conta == 'z':
+            exit()
+        else:
+            nome = CriarConta(tatu)
+
+        Menu(tatu, tatu.ProcurarConta(nome))
 
 main()
