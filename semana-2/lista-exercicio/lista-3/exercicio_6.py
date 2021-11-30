4d6f25b2335ad07d6d495ce8e01a938f909ffa0c
Larissa Justen <larissajusten@gmail.com>
2021-11-30 12:05:49 -0300

    Add lista 3 e exerc 6

--- semana-2/lista-exercicio/lista-3/exercicio_6.py
+++ semana-2/lista-exercicio/lista-3/exercicio_6.py
@@ -1,3 +1,18 @@
 """
     Exercício 6. Criar 10 frozensets com 30 números aleatórios cada, e construir um dicionário que contenha a soma de cada um deles.
 """
+import random
+
+def get_random_set(size):
+    return frozenset(random.sample(range(1, 100), size))
+
+def get_random_sets(size, num_sets):
+    return [get_random_set(size) for _ in range(num_sets)]
+
+def get_dict_from_sets_sum(sets):
+    return {key: sum(value) for key, value in enumerate(sets)}
+
+if __name__ == '__main__':
+    _sets = get_random_sets(30, 10)
+    _dict = get_dict_from_sets_sum(_sets)
+    print(_dict)
